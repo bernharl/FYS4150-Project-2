2cd434c3882a4a3893e1e805268f358f3b5f6cb8
Nils-Ole Stutzer <nilsole2009@live.no>
2019-09-13 13:41:42 +0200

    Jacobi's algo now returns right eigenvalues, cleanup in script needed

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,6 @@
 using namespace std;
 
 
-inline double tau(double a_ll, double a_kk, double a_kl)
-{
-  return (a_ll - a_kk) / (2. * a_kl);
-}
-
-
-inline double t(double tau)
-{
-  double t = 1.0;
-  if(tau > 0)
-  {
-    t /= (tau + sqrt(1. + tau * tau));
-  }
-  else
-  {
-    t /= (- (-tau + sqrt(1. + tau * tau)));
-  }
-  return t;
-}
-
-inline double c(double t)
-{
-  return 1. / sqrt(1. + t * t);
-}
-
-inline double s(double c, double t)
-{
-  return t * c;
-}
-
 double max_offdiag(const arma::mat &A, int &k, int &l, int n)
 {
   double max_val = 0.0;
@@ -47,17 +17,15 @@ double max_offdiag(const arma::mat &A, int &k, int &l, int n)
       }
     }
   }
-  cout << k << " " << l  << endl;
   return max_val;
 }
 
+
 int main()
 {
-  //arma::mat A = arma::mat(n, n, arma::fill::zeros);
-  arma::mat zero_diag_matrix = arma::ones <arma::mat> (n, n);
-  zero_diag_matrix.diag(0).fill(0);
+ 
   arma::mat A = arma::zeros <arma::mat> (n, n);
-  arma::mat B = arma::zeros <arma::mat> (n, n);
+  arma::mat B;
   A.diag(0).fill(d);
   A.diag(1).fill(a);
   A.diag(-1).fill(a);
@@ -70,27 +38,33 @@ int main()
   double max_val = max_offdiag(A, k, l, n);
   cout << k << " " << l << endl;
   double a_ll, a_kk, a_ik, a_il, a_kl;//, a_lk;
-  cout << max_val << " " << k << " " << l << endl;
+  //cout << max_val << " " << k << " " << l << endl;
 
-  //a_kl = A(k, l);
-  //a_ll = A(l, l);
-  //a_kk = A(k, k);
-  //cout << A << endl;
+  double t_val, tau_val, c_val, s_val; 
   while(max_val * max_val > eps)
   {
     a_kl = A(k, l);
     a_ll = A(l, l);
     a_kk = A(k, k);
 
-    double tau_val = tau(a_ll, a_kk, a_kl);
-    double t_val = t(tau_val);
-    double c_val = c(t_val);
-    double s_val = s(c_val, t_val);
+    tau_val = (a_ll - a_kk) / (2. * a_kl); // tau(a_ll, a_kk, a_kl);
+    if(tau_val > 0)
+    {
+      t_val = 1.0 / (tau_val + sqrt(1. + tau_val * tau_val));
+    }
+    else
+    {
+      t_val = -1.0 / (-tau_val + sqrt(1. + tau_val * tau_val));
+    }
+    c_val = 1. / sqrt(1. + t_val * t_val); //c(t_val);
+    s_val = t_val * c_val; // s(c_val, t_val);
 
-    B = A;
-    //a_kl = A(k, l);
-    //a_ll = A(l, l);
-    //a_kk = A(k, k);
+    // A = A;
+    //cout << "hei " << a_kk << " " << a_ll << endl;
+    //cout << "hei2 " << a_kk << " " << a << endl;
+    A(k, k) = a_kk * c_val * c_val - 2.0 * a_kl * c_val * s_val + a_ll * s_val * s_val;
+    A(l, l) = a_ll * c_val * c_val + 2.0 * a_kl * c_val * s_val + a_kk * s_val * s_val;
+    A(l, k) = A(k, l) = 0;
 
     for (int i = 0; i < n; i++)
     {
@@ -98,21 +72,18 @@ int main()
       a_il = A(i, l);
       if (i != k && i != l)
       {
-        //B(i, i) = A(i, i);
-        B(i, k) = a_ik * c_val - a_il * s_val;
-        B(i, l) = a_il * c_val + a_ik * s_val;
+        A(i, k) = A(k, i) = a_ik * c_val - a_il * s_val;
+        A(i, l) = A(l, i) = a_il * c_val + a_ik * s_val;
       }
-      B(k, k) = a_kk * c_val * c_val - 2 * a_kl * c_val * s_val + a_ll * s_val * s_val;
-      B(l, l) = a_ll * c_val * c_val + 2 * a_kl * c_val * s_val + a_kk * s_val * s_val;
-      B(l, k) = B(k, l) = 0;
-      cout << (a_kk - a_ll) * c_val * s_val + a_kl * (c_val * c_val - s_val * s_val) << endl;
 
     }
+
+    // A = A;
     //cout << A << endl;
-    A = B;
     max_val = max_offdiag(A, k, l, n);
-    cout << max_val << endl;
+    cout << "Max value " << max_val << endl;
   }
-  cout << A << endl;
+    cout << A << endl;
   return 0;
 }
+
