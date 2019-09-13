59acfd39fd793ed58cb631ebcfc5a812eeb4fb39
bernharl <bernharl@student.matnat.uio.no>
2019-09-13 12:03:05 +0200

    Something is wrong witht he rotation algorithm

--- src/keyword_parameters.h
+++ src/keyword_parameters.h
@@ -1,5 +1,5 @@
 int n = 5;
 double h = 1. / (double) n;
-double eps = 1e-8;
-double d = 2. / (h * h);
-double a = -1. / (h * h);
+double eps = 0.1;
+double d = 2. / (h * h)* 0 + 2.;
+double a = -1. / (h * h) * 0 +1.;
--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ inline double t(double tau)
   }
   else
   {
-    t /= - (-tau + sqrt(1. + tau * tau));
+    t /= (- (-tau + sqrt(1. + tau * tau)));
   }
   return t;
 }
@@ -36,7 +36,7 @@ inline double s(double c, double t)
 }
 
 double max_offdiag(const arma::mat &A, int &k, int &l, int n)
-{ 
+{
   double max_val = 0.0;
   for (int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
@@ -47,7 +47,7 @@ double max_offdiag(const arma::mat &A, int &k, int &l, int n)
       }
     }
   }
-  cout << "jeff" << max_val << endl;
+  cout << k << " " << l  << endl;
   return max_val;
 }
 
@@ -61,46 +61,57 @@ int main()
   A.diag(0).fill(d);
   A.diag(1).fill(a);
   A.diag(-1).fill(a);
+  arma::cx_vec eig_val;
+  arma::cx_mat eig_vec;
+  arma::eig_gen(eig_val, eig_vec, A);
+  cout << eig_val << endl;
   int k, l;
 
   double max_val = max_offdiag(A, k, l, n);
-  //cout << max_val << endl;
+  cout << k << " " << l << endl;
   double a_ll, a_kk, a_ik, a_il, a_kl;//, a_lk;
   cout << max_val << " " << k << " " << l << endl;
 
-  a_kl = A(k, l);
-  //a_lk = A(l, k);
+  //a_kl = A(k, l);
+  //a_ll = A(l, l);
+  //a_kk = A(k, k);
   //cout << A << endl;
   while(max_val * max_val > eps)
-  { 
-    
-    
+  {
+    a_kl = A(k, l);
+    a_ll = A(l, l);
+    a_kk = A(k, k);
+
     double tau_val = tau(a_ll, a_kk, a_kl);
     double t_val = t(tau_val);
     double c_val = c(t_val);
     double s_val = s(c_val, t_val);
-    
+
     B = A;
-    a_ll = A(l, l);
-    a_kk = A(k, k);
-  
+    //a_kl = A(k, l);
+    //a_ll = A(l, l);
+    //a_kk = A(k, k);
+
     for (int i = 0; i < n; i++)
-    { 
+    {
       a_ik = A(i, k);
       a_il = A(i, l);
       if (i != k && i != l)
       {
-        B(i, i) = A(i, i);
+        //B(i, i) = A(i, i);
         B(i, k) = a_ik * c_val - a_il * s_val;
         B(i, l) = a_il * c_val + a_ik * s_val;
       }
       B(k, k) = a_kk * c_val * c_val - 2 * a_kl * c_val * s_val + a_ll * s_val * s_val;
       B(l, l) = a_ll * c_val * c_val + 2 * a_kl * c_val * s_val + a_kk * s_val * s_val;
-      B(l, k) = B(k, l) = (a_kk - a_ll) * c_val * s_val + a_kl * (c_val * c_val - s_val * s_val);
-      A = B;
+      B(l, k) = B(k, l) = 0;
+      cout << (a_kk - a_ll) * c_val * s_val + a_kl * (c_val * c_val - s_val * s_val) << endl;
+
     }
     //cout << A << endl;
-    max_val = max_offdiag(A, k, l, n);  
+    A = B;
+    max_val = max_offdiag(A, k, l, n);
+    cout << max_val << endl;
   }
   cout << A << endl;
   return 0;
