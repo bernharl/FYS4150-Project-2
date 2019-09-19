2be43dde66db8828dcb01ef1854df23b613844f5
Nils-Ole Stutzer <nilsole2009@live.no>
2019-09-19 11:40:09 +0200

    Now we have four digit precision in 4 first eigenvalues of single electron problem

--- src/keyword_parameters.h
+++ src/keyword_parameters.h
@@ -1,8 +1,9 @@
-int n = 200;
+int n = 400;
 double rho0 = 0;
-double rhoN = 4.3;
-double h = (rhoN - rho0) / ((double) n);
+double rhoN = 5.4;
+double h = (rhoN - rho0) / ((double) n + 1);
 double eps = 1e-8;
 double d = 2. / (h * h);
 double a = -1. / (h * h);
+int tot_iterations = 3 * n * n;
 
