0a20e3033bc77a897adeedbec772aa10bcd157a2
bernharl <bernharl@student.matnat.uio.no>
2019-09-27 10:53:48 +0200

    Updated plot script

--- src/plot_infinity.py
+++ src/plot_infinity.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 # Setting fonts for pretty plot
 fonts = {
-        "font.family": "serif",
-        "axes.labelsize": 10,
-        "font.size": 10,
-        "legend.fontsize": 10,
-        "xtick.labelsize": 10,
-        "ytick.labelsize": 10,
+    "font.family": "serif",
+    "axes.labelsize": 10,
+    "font.size": 10,
+    "legend.fontsize": 10,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
 }
 plt.rcParams.update(fonts)
 
@@ -22,14 +23,20 @@ fig.set_size_inches(2 * 2.9, 2 * 1.81134774961)
 
 # Plotting dats
 for i, eigen_error in enumerate(errors.T):
-    ax.semilogy(data[:, 0], eigen_error, label=fr"|Err(Eigen={expected[i]})|")
+    ax.semilogy(data[:, 0], eigen_error, label=fr"|Err($\lambda$={expected[i]})|")
 
 # We want errors below 1e-3
 error_lim = np.ones_like(data[:, 0]) * 1e-3
 ax.plot(data[:, 0], error_lim, label="Four leading digits")
-#Finding best RhoN for lambda=15
+# Finding best RhoN for lambda=15
 min_pos = np.argmin(errors[:, -1])
-ax.scatter(data[min_pos, 0], errors[min_pos, -1], label="Best")
+# ax.scatter(data[min_pos, 0], errors[min_pos, -1], label="Best")
+ax.plot(
+    (data[min_pos, 0], data[min_pos, 0]),
+    (0, np.max(errors[:, -1])),
+    "--",
+    label=rf"Best $\rho_{{max}} \approx {data[min_pos, 0]:.2f}$",
+)
 ax.grid()
 ax.set_xlabel(r"$\rho_{max}$")
 ax.set_ylabel(r"Absolute error")
